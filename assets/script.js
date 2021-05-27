2682cf2ede95c4a65b253381869237e32cf250a4
Shreekz <alanmguadet@gmail.com>
2021-05-26 21:49:41 -0400

    Day forecast done, hmtl on 5-day implemented

--- assets/script.js
+++ assets/script.js
@@ -12,8 +12,12 @@
 
 var buttonEl = document.querySelector('button');
 var inputEl = document.querySelector('input');
-var sectionEl = document.querySelector('section');
-var ulEl = document.querySelector('ul');
+var sectionEl = document.querySelector('.search-bar');
+var ulEl = document.querySelector('.history');
+var resultsEl = document.querySelector('.results');
+var detailsEl = document.querySelector('.details');
+var weatherIcon = document.querySelector('#icon');
+var weekForecast = document.querySelector('h3');
 
 var getApi = function(city) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=640e0b3fa5888812105e65613290ab54"
@@ -23,11 +27,59 @@ var getApi = function(city) {
             return response.json();
         })
         .then(function (weather) {
-            console.log(weather);
+            renderWeather(weather);
+            getForecast(weather.coord.lon, weather.coord.lat) 
         })
+        
 }
 
-sectionEl.addEventListener("click", function() {
+var getForecast = function(lon, lat) {
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat +'&lon=' + lon + '&appid=640e0b3fa5888812105e65613290ab54'
+    console.log(requestUrl);
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (weather) {
+            renderWeekForecast(weather);
+        })
+
+
+}
+
+var renderWeather = function(weather){
+    console.log(weather);
+    var icon = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
+    var date = new Date (weather.dt * 1000);
+    weatherIcon.setAttribute("src", icon);
+    resultsEl.textContent = weather.name + " " + date.toDateString(); 
+
+    var temp = document.createElement('li');
+    temp.textContent = "Temp: " + weather.main.temp;
+    var wind = document.createElement('li');
+    wind.textContent = "Wind: " + weather.wind.speed + " MPH";
+    var humidity = document.createElement('li');
+    humidity.textContent = "Humidity: " + weather.main.humidity + " %";
+ 
+    detailsEl.appendChild(temp);
+    detailsEl.appendChild(wind);
+    detailsEl.appendChild(humidity);
+}
+
+var renderWeekForecast = function(forecast) {
+    var uvi = document.createElement('li');
+    uvi.textContent = "UV Index: " + forecast.current.uvi;
+    detailsEl.appendChild(uvi);
+    weekForecast.textContent = "Five-Day Forecast";
+
+    
+
+
+
+}
+
+sectionEl.addEventListener("click", function(event) {
     var element = event.target;
     if (element.matches('button')) {
         event.preventDefault();
