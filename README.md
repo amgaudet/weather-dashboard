e43441c10af283d9c34d0ab78d9f62aa085f17b6
Shreekz <alanmguadet@gmail.com>
2021-05-27 22:30:50 -0400

    minor css stylings, fixed over population for consecutive searches

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# weather-dashboard
+# Weather Dashboard
+## Contents
+1. [About](#About)
+2. [Description](#Description)
+3. [Screenshot](#Screenshot)
+4. [Contact-Info](#Contact-Info)
+3. [License](#License)
+
+## About
+Application prompts user to search for a city on the side search bar. Once submitted, the main portion of the screen will load out the current weather for said location with color coded UV Index. It will also display a 5-day outlook. The application will also save recent searches.
+
+## Description
+[Deployed Application](https://amgaudet.github.io/weather-dashboard/)
+On load, application checks local storage for recent searches and creates a drop down button list of any searches stored in local storage. The application then fetches weather conditions from the openweathermap API. Weather conditions are then populated with a list of details. Any searches after will clear the screen and reload with the new city searched. The user can also click on any of the recent search buttons to re-do a search on a saved city.
+
+## Screenshot
+<img src="./assets/weather-dashboard-preview.png" width="600" height="auto">
+
+## Contact-Info
+
+Alan Gaudet - <alanmgaudet@gmail.com>
+
+Portfolio - <https://github.com/amgaudet>
+
+## License
+
+MIT License
+
+Copyright (c) 2021 Alan Gaudet
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,3 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
 var buttonEl = document.querySelector('button');
 var inputEl = document.querySelector('input');
 var sectionEl = document.querySelector('.search-bar');
@@ -17,8 +5,10 @@ var ulEl = document.querySelector('.history');
 var resultsEl = document.querySelector('.results');
 var detailsEl = document.querySelector('.details');
 var weatherIcon = document.querySelector('#icon');
-var fiveDayForecast = document.querySelector('h3');
 var weekForecast = document.querySelector('.week-forecast');
+var searchHistory = JSON.parse(localStorage.getItem("history")) || [];
+var containerEl = document.querySelector('.container');
+var entryEl = document.querySelector('#entry');
 
 var getApi = function (city) {
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=640e0b3fa5888812105e65613290ab54"
@@ -44,11 +34,17 @@ var getForecast = function (lon, lat) {
         .then(function (weather) {
             renderWeekForecast(weather);
         })
-
-
 }
 
+var clearHistory = function (parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+    return;
+};
+
 var renderWeather = function (weather) {
+    clearHistory(detailsEl);
     var icon = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png"
     var date = new Date(weather.dt * 1000);
     weatherIcon.setAttribute("src", icon);
@@ -64,18 +60,31 @@ var renderWeather = function (weather) {
     detailsEl.appendChild(temp);
     detailsEl.appendChild(wind);
     detailsEl.appendChild(humidity);
-}
+};
 
 var renderWeekForecast = function (forecast) {
-    console.log(forecast);
+    clearHistory(weekForecast);
+
     var forecastContainer = document.createElement('span');
+    forecastContainer.setAttribute("class", "week-forecast");
     var uvi = document.createElement('li');
     uvi.textContent = "UV Index: " + forecast.current.uvi;
+    uvValue = parseInt(forecast.current.uvi);
+    if (uvValue < 3) {
+        uvi.setAttribute("class", "green");
+    } else if (uvValue < 6) {
+        uvi.setAttribute("class", "yellow");
+    } else {
+        uvi.setAttribute("class", "red");
+    }
+
     detailsEl.appendChild(uvi);
 
+    var fiveDayForecast = document.createElement('h3');
     fiveDayForecast.textContent = "Five-Day Forecast";
+    weekForecast.appendChild(fiveDayForecast);
     weekForecast.appendChild(forecastContainer);
-    
+
 
     for (var i = 0; i < 5; i++) {
         var dayCast = document.createElement('div');
@@ -83,11 +92,11 @@ var renderWeekForecast = function (forecast) {
 
         forecastContainer.append(dayCast);
         dayCast.appendChild(forecastList);
- 
+
         var dateEl = document.createElement('li');
         var date = new Date(forecast.daily[i].dt * 1000);
         dateEl.textContent = date.toDateString();
-        
+
         var icon = "https://openweathermap.org/img/w/" + forecast.daily[i].weather[0].icon + ".png"
         var iconHolder = document.createElement('li');
         var iconEl = document.createElement('img');
@@ -107,26 +116,50 @@ var renderWeekForecast = function (forecast) {
         dayCast.appendChild(wind);
         dayCast.appendChild(humidity);
     }
+};
 
-
-
-}
-
-sectionEl.addEventListener("click", function (event) {
+entryEl.addEventListener("click", function (event) {
     var element = event.target;
     if (element.matches('button')) {
         event.preventDefault();
 
         var search = inputEl.value;
         var newLiEl = document.createElement('li');
-        var searchHistory = document.createElement('button');
-        searchHistory.textContent = search;
+        var searchBtnEl = document.createElement('button');
+        searchBtnEl.textContent = search;
 
-        ulEl.append(newLiEl);
-        newLiEl.append(searchHistory);
+        if (!searchHistory.includes(search)) {
+            searchHistory = searchHistory.concat(search);
+            localStorage.setItem("history", JSON.stringify(searchHistory));
+
+            ulEl.append(newLiEl);
+            newLiEl.append(searchBtnEl);
+        }
 
         getApi(search);
     }
 });
 
+ulEl.addEventListener("click", function (event) {
+    var element = event.target;
+    if (element.matches('button')) {
+        var search = element.textContent;
+        getApi(search);
+    }
+});
 
+
+
+var init = function () {
+    for (var i = 0; i < searchHistory.length; i++) {
+        var search = searchHistory[i];
+        var newLiEl = document.createElement('li');
+        var searchBtnEl = document.createElement('button');
+        searchBtnEl.textContent = search;
+
+        ulEl.append(newLiEl);
+        newLiEl.append(searchBtnEl);
+    }
+};
+
+init();
